2bc6da2bb7bfba7300a002e94279e9e4ab01a658
king2426 <themelonhead14@gmail.com>
2019-06-24 11:36:49 -0500

    Created controllers for admin and user webpages for navigation. Also EagleEye.csproj got changed because of the added controllers.

---
+++ EagleEye/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EagleEye.Controllers
+{
+	public class UserController : Controller
+	{
+        public ActionResult UserHome()
+        {
+            ViewBag.Message = "Your Home Page";
+            return View();
+        }
+		public ActionResult Lot()
+		{
+			ViewBag.Message = "Your user Lot page.";
+
+			return View();
+		}
+	}
+}
