a7e60df02553b60b34456a5ee2001a4b1a07995d
Gage Coates <sg.p4x347@gmail.com>
2019-06-29 16:56:03 -0700

    Added occupation to Annotaiton Model and ViewModel

--- EagleEye/Views/ParkingLot/Annotation.cs
+++ EagleEye/Views/ParkingLot/Annotation.cs
@@ -13,9 +13,11 @@ namespace EagleEye.Views.ParkingLot
 			ID = annotation.ID;
 			Type = annotation.Type.ToString();
 			Points = annotation.Points.Select(p => new Vector2(p)).ToList();
+			Occupied = annotation.Occupied;
 		}
 		public int ID { get; set; }
 		public string Type { get; set; }
+		public double Occupied { get; set; }
 		public List<Vector2> Points { get; set; }
 	}
 }
