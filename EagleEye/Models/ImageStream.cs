1102bf70244afd88cb05747a4f51fb32c662c374
sg_p4x347 <sg.p4x347@gmail.com>
2019-07-05 18:28:25 -0500

    A* pathfinding funciton declarations added

---
+++ EagleEye/Models/ImageStream.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EagleEye.Models
+{
+	public class ImageStream : HttpRequestBase
+	{
+		public ImageStream()
+		{
+		}
+		System.IO.Stream Stream { get => InputStream; }
+	}
+}
